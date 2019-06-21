0b24b937f2906958cd22aa1de35864577aa5be77
1287237688 <51707476+1287237688@users.noreply.github.com>
2019-06-21 23:57:51 +0800

    Initial commit

---
+++ zf.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+struct Student
+{
+int num;
+char name[20];
+float score;
+};
+int main()
+{
+struct Student stu[5]={{10101,"Zhang",78},{10103,"wang",98},{10106,"Li",86},{10108,"Ling",73.5},{10110,"Sun",100}};
+struct Student temp;
+const int n=5;
+int i,j,k;
+printf("The order is:\n");
+for(i=0;i<n-1;i++)
+{
+k=i;
+for(j=i+1;j<n;j++)
+if(stu[j].score>stu[k].score )
+k=j;
+temp=stu[k];
+stu[k]=stu[i];
+stu[i]=temp;
+}
+for(i=0;i<n;i++)
+printf("%6d  %8s  %6.2f\n",stu[i].num,stu[i].name,stu[i].score);
+return 0;
+}
